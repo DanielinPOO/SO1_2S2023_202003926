f314d643df1456331e3601dc280db8dd234e4be1
DanielinPOO <danielminchez@gmail.com>
2023-10-21 12:58:16 -0600

    Tarea7

---
+++ Tareas/Tarea7/Clase gRPC/client/Main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"context"
+	pb "ejemplo_despliegue/grpcClient"
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var ctx = context.Background()
+
+type Data struct {
+	Carnet   string
+	Nombre   string
+	Curso    string
+	Nota     string
+	Semestre string
+	Year     string
+}
+
+func insertData(c *fiber.Ctx) error {
+	var data map[string]string
+	e := c.BodyParser(&data)
+	if e != nil {
+		return e
+	}
+
+	rank := Data{
+		Carnet:   data["carnet"],
+		Nombre:   data["nombre"],
+		Curso:    data["curso"],
+		Nota:     data["nota"],
+		Semestre: data["semestre"],
+		Year:     data["year"],
+	}
+
+	go sendRedisServer(rank)
+	//go sendMysqlServer(rank)
+
+	return nil
+}
+
+func sendRedisServer(rank Data) {
+	conn, err := grpc.Dial("localhost:3001", grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock())
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	cl := pb.NewGetInfoClient(conn)
+	defer func(conn *grpc.ClientConn) {
+		err := conn.Close()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}(conn)
+
+	ret, err := cl.ReturnInfo(ctx, &pb.RequestId{
+		Carnet:   rank.Carnet,
+		Nombre:   rank.Nombre,
+		Curso:    rank.Curso,
+		Nota:     rank.Nota,
+		Semestre: rank.Semestre,
+		Year:     rank.Year,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("Respuesta del server " + ret.GetInfo())
+}
+
+func sendMysqlServer(rank Data) {
+
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"res": "todo bien",
+		})
+	})
+	app.Post("/insert", insertData, func(c *fiber.Ctx) error {
+		data := struct {
+			Message string `json:"message"`
+			Status  int    `json:"status"`
+		}{
+			Message: "Hola, mundo",
+			Status:  200,
+		}
+
+		return c.JSON(data)
+	})
+
+	err := app.Listen(":3000")
+	if err != nil {
+		return
+	}
+}
---
+++ Tareas/Tarea7/Clase gRPC/server/serv.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	pb "golangSocket/grpc-server"
+	"log"
+	"net"
+
+	_ "github.com/go-sql-driver/mysql"
+	"google.golang.org/grpc"
+)
+
+var ctx = context.Background()
+var db *sql.DB
+
+type server struct {
+	pb.UnimplementedGetInfoServer
+}
+
+const (
+	port = ":3001"
+)
+
+type Data struct {
+	Carnet   string
+	Nombre   string
+	Curso    string
+	Nota     string
+	Semestre string
+	Year     string
+}
+
+func mysqlConnect() {
+	// Cambia las credenciales según tu configuración de MySQL
+	dsn := "root:HU&8\\O7QDqH5$&/*@tcp(35.237.25.189)/proyecto2"
+
+	var err error
+	db, err = sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("Conexión a MySQL exitosa")
+}
+
+func (s *server) ReturnInfo(ctx context.Context, in *pb.RequestId) (*pb.ReplyInfo, error) {
+	fmt.Println("Recibí de cliente: ", in.GetCarnet())
+	data := Data{
+		Carnet:   in.GetCarnet(),
+		Nombre:   in.GetNombre(),
+		Curso:    in.GetCurso(),
+		Nota:     in.GetNota(),
+		Semestre: in.GetSemestre(),
+		Year:     in.GetYear(),
+	}
+	insertMySQL(data)
+	return &pb.ReplyInfo{Info: "Hola cliente, recibí el comentario"}, nil
+}
+
+func insertMySQL(rank Data) {
+	// Prepara la consulta SQL para la inserción en MySQL
+	query := "INSERT INTO calificacion (carnet, nombre, curso, nota, semestre, year) VALUES (?, ?, ?, ?, ?, ?)"
+	_, err := db.ExecContext(ctx, query, rank.Carnet, rank.Nombre, rank.Curso, rank.Nota, rank.Semestre, rank.Year)
+	if err != nil {
+		log.Println("Error al insertar en MySQL:", err)
+	}
+}
+
+func main() {
+	listen, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGetInfoServer(s, &server{})
+
+	mysqlConnect()
+
+	if err := s.Serve(listen); err != nil {
+		log.Fatalln(err)
+	}
+}
